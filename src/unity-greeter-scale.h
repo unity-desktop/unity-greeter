4ee3ac92a837db06b363d45c54306b5761b0a952
Muqtxdir <muqtxdir@gmail.com>
2026-09-10 16:16:36 +0530

    feat (scale): copy and expose mutter's calc for scaling

---
+++ src/unity-greeter-scale.h
@@ -0,0 +1,16 @@
+/* unity-greeter-scale.h
+ *
+ * Copyright 2026 Muqtadir
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+#include <glib.h>
+
+G_BEGIN_DECLS
+
+gchar *unity_greeter_scale_compose (void);
+
+G_END_DECLS
