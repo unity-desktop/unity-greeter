8986833d2b7b1047fe28a7499dd992308e1fb564
Muqtxdir <muqtxdir@gmail.com>
2026-09-10 16:29:03 +0530

    feat: replace phoc with wayfire and simplify conf handling

--- src/unity-greeter-session.c
+++ src/unity-greeter-session.c
@@ -2,19 +2,6 @@
  *
  * Copyright 2026 Muqtadir
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
@@ -23,52 +10,24 @@
 #include <unistd.h>
 
 #include <glib.h>
-#include <glib/gstdio.h>
 
-static gchar *
-newest_phoc_config (void)
-{
-  g_autoptr (GDir) dir = g_dir_open (UNITY_GREETER_MIRROR_ROOT, 0, NULL);
-  if (dir == NULL)
-    return NULL;
-
-  gchar       *best = NULL;
-  gint64       best_mtime = 0;
-  const gchar *name;
-
-  while ((name = g_dir_read_name (dir)) != NULL)
-    {
-      g_autofree gchar *ini =
-        g_build_filename (UNITY_GREETER_MIRROR_ROOT, name, "phoc.ini", NULL);
-      GStatBuf st;
-      if (g_stat (ini, &st) != 0)
-        continue;
-      if (best == NULL || st.st_mtime > best_mtime)
-        {
-          g_free (best);
-          best = g_steal_pointer (&ini);
-          best_mtime = st.st_mtime;
-        }
-    }
-  return best;
-}
+#include "unity-greeter-scale.h"
 
 gint
 main (void)
 {
   g_setenv ("GDK_BACKEND",   "wayland",      TRUE);
-  g_setenv ("DCONF_PROFILE", "greetd",       TRUE);
   g_setenv ("WLR_BACKENDS",  "drm,libinput", TRUE);
 
-  g_autofree gchar *config = newest_phoc_config ();
+  g_autofree gchar *config = unity_greeter_scale_compose ();
+  if (config == NULL)
+    {
+      g_printerr ("unity-greeter-session: cannot compose wayfire config\n");
+      return EXIT_FAILURE;
+    }
 
-  if (config != NULL)
-    execlp ("phoc", "phoc", "-C", config,
-            "--no-xwayland", "-E", "unity-greeter", (gchar *) NULL);
-  else
-    execlp ("phoc", "phoc",
-            "--no-xwayland", "-E", "unity-greeter", (gchar *) NULL);
+  execlp ("wayfire", "wayfire", "-c", config, (gchar *) NULL);
 
-  g_printerr ("unity-greeter-session: exec phoc: %s\n", g_strerror (errno));
+  g_printerr ("unity-greeter-session: exec wayfire: %s\n", g_strerror (errno));
   return EXIT_FAILURE;
 }
